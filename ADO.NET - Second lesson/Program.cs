00bfc6c6b0149b86013514f30679e781eb0b2788
Jurayevkh <jurayevkh@gmail.com>
2023-10-26 18:37:08 +0500

    ADO.NET - lesson and HomeWork added

---
+++ ADO.NET - Second lesson/Program.cs
@@ -0,0 +1,66 @@
+﻿using Npgsql;
+
+Database.GetAll("fruits","something","id>8");
+Database.DeleteData("fruits", "id>=7","something");
+Console.ReadLine();
+
+public class Database
+{
+
+    public static void GetAll(string TableName, string Database, string Condition)
+
+    {
+        using (NpgsqlConnection connection = new NpgsqlConnection($"Server=localhost;Port=5433;Database={Database};User Id=postgres;Password=20081403;"))
+        {
+            connection.Open();
+            string query = $"select * from {TableName} where {Condition}";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            NpgsqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]} {reader[3]}");
+            }
+
+            reader.Close();
+        }
+
+    }
+
+    public static void DeleteData(string TableName,string Condition, string Database)
+    {
+        using(NpgsqlConnection connection =new NpgsqlConnection($"Server=localhost;Port=5433;Database={Database};User Id=postgres;Password=20081403;"))
+        {
+            connection.Open();
+            string query = $"Delete from {TableName} where {Condition}";
+
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+            Console.WriteLine("Malumot o'chirildi :)");
+            reader.Close();
+        }
+    }
+
+    //public static void InsertData(string TableName,Fruit fruit)
+    //{
+    //    using (NpgsqlConnection connection = new NpgsqlConnection($"Server=localhost;Port=5433;Database=something;User Id=postgres;Password=20081403;"))
+    //    {
+    //        connection.Open();
+    //        string query = $"";
+    //    }
+
+
+    //}
+
+}
+
+
+public class Fruit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float price { get; set; }
+    public int count { get; set; }
+}
---
+++ ADO.NET/Program.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+
+string connectionString = "Server=localhost;Port=5433;Database=something;User Id=postgres;Password=20081403;";
+NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+
+//connection.ConnectionString = "Server=localhost;Database=postgres;User Id=postgres;Password=20081403;";
+////Server=<server>;Port=<port>;Database=<database>;User Id=<username>;Password=<password>;
+connection.Open();
+string query = "select * from fruits";
+
+NpgsqlCommand command = new NpgsqlCommand(query, connection);
+
+NpgsqlDataReader reader = command.ExecuteReader();
+
+while (reader.Read())
+{
+    Console.WriteLine(reader[1]);
+}
+
+//using(SqlDataReader reader= command.ExecuteReader())
+//{
+//    while (reader.Read())
+//    {
+//        Console.WriteLine($"{reader[0]} {reader[1]} {reader[2]} {reader[3]}");
+//    }
+//}
+
+Console.ReadLine();
+
