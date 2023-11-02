dada40d5082d4f8bfcfc43380efae7fe1b850a0f
Jurayevkh <jurayevkh@gmail.com>
2023-11-02 19:37:50 +0500

    HomeWork Added

---
+++ SearchDb.WebApi/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SearchDb.WebApi.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SearchDb.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly string connectionString = WebApplication.CreateBuilder().Configuration.GetConnectionString("Connect");
+
+        [HttpGet]
+        public IActionResult SearchValue(string searchStr)
+        {
+            using ( SqlConnection connection = new SqlConnection(connectionString))
+            {
+                    string query = $"EXEC SearchProcedure {searchStr}";
+                    var result = connection.Query<Search>(query);
+                     return Ok(result);
+            }
+
+        }
+    }
+}
